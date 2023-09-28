4550fd20248a210159a731c4f9e40b2f94025c2b
Olorode Durotimi <rotimiiam@gmail.com>
2023-09-28 23:55:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(express.json());
 const videoRoutes = require('./routes/video');
 
 // Use the video route
-app.use('/video', videoRoutes);
-app.use('/upload', videoRoutes);
+app.use('/', videoRoutes);
+app.use('/', videoRoutes);
 
 
 // Start the server
