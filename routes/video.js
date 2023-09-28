8a59ce543eaf452c77bc4a19a946d455de1e70df
Olorode Durotimi <rotimiiam@gmail.com>
2023-09-29 00:22:01 +0100

    Update video.js

--- routes/video.js
+++ routes/video.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer'); // For handling file uploads
+const fs = require('fs'); // For working with the file system
 
 const router = express.Router();
 
@@ -26,11 +27,23 @@ router.post('/upload', upload.single('video'), (req, res) => {
     // Get the uploaded file as a buffer
     const videoBuffer = req.file.buffer;
 
-    // You can now do whatever you want with the videoBuffer
-    // For example, you can send it back to the client for them to save
-    res.setHeader('Content-Disposition', 'attachment; filename="screen_rec.mp4"');
-    res.setHeader('Content-Type', 'video/mp4');
-    res.send(videoBuffer);
+    // Define the path to save the video file to the "uploads" folder
+    const filePath = './uploads/screen_rec.mp4';
+
+    // Write the video buffer to the file system
+    fs.writeFile(filePath, videoBuffer, (err) => {
+      if (err) {
+        console.error('Error saving video to disk:', err);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+      // Set the response headers for video playback
+      res.setHeader('Content-Disposition', 'attachment; filename="screen_rec.mp4"');
+      res.setHeader('Content-Type', 'video/mp4');
+
+      // Send the video buffer as the response
+      res.send(videoBuffer);
+    });
   } catch (error) {
     console.error('Error handling video upload:', error);
     res.status(500).json({ message: 'Internal server error' });
