b2638de6b227d91372cd2f1b763ecd72798c0d4f
Olorode Durotimi <rotimiiam@gmail.com>
2023-09-29 11:44:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require('express'); 
 const app = express();
 const port = process.env.PORT || 3000; // Use the provided port or default to 8000
 
+app.use(cors());
 // Middleware to parse JSON requests
 app.use(express.json());
 
